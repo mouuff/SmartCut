43bd328938b8e7eafe488fb8b9788fd1ca5baa15
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-17 08:26:55 +0200

    Create orchestrator.go

---
+++ pkg/orchestrator/orchestrator.go
@@ -0,0 +1,76 @@
+package orchestrator
+
+import (
+	"context"
+	"log"
+
+	"golang.design/x/clipboard"
+)
+
+type Brain interface {
+	GenerateString(ctx context.Context, propertyName, prompt string) (string, error)
+}
+
+type GenerationResult struct {
+	Text string
+}
+
+type GenerationRequest struct {
+	Prompt       string
+	PropertyName string
+}
+
+type Orchestrator struct {
+	Context context.Context
+	Brain   Brain
+	In      chan GenerationRequest
+	Out     chan GenerationResult
+}
+
+func NewOrchestrator() *Orchestrator {
+	return &Orchestrator{
+		In:  make(chan GenerationRequest),
+		Out: make(chan GenerationResult),
+	}
+}
+
+func (o *Orchestrator) Start() {
+	go func() {
+		for req := range o.In {
+			// Process the request (placeholder logic)
+			result, err := o.Brain.GenerateString(o.Context, req.PropertyName, req.Prompt)
+
+			if err != nil {
+				log.Println("Error generating:", err)
+				continue
+			} else {
+				o.Out <- GenerationResult{
+					Text: result,
+				}
+			}
+		}
+	}()
+}
+
+func (o *Orchestrator) StartFeedFromClipboard() error {
+	// Init returns an error if the package is not ready for use.
+	err := clipboard.Init()
+	if err != nil {
+		return err
+	}
+
+	ch := clipboard.Watch(context.TODO(), clipboard.FmtText)
+	for data := range ch {
+		// print out clipboard data whenever it is changed
+		println(string(data))
+
+		o.In <- GenerationRequest{
+			Prompt:       string(data),
+			PropertyName: "result",
+		}
+	}
+
+	log.Println("Stopped feeding from clipboard.")
+
+	return nil
+}
