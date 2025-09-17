25cd90bfca0ed129c513fe217129e18e97accc10
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-17 18:33:55 +0200

    Add zip packaging to build script and update .gitignore
    Updated .gitignore to exclude .zip files. Modified build.sh to remove any existing binaries.zip and create a new zip archive containing the built executable.

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 binary_name=$(go run cmd/smartcut/main.go -printBin=true)
 go build -ldflags -H=windowsgui cmd/smartcut/main.go
 mv main.exe $binary_name.exe
+rm binaries.zip
+zip -r binaries.zip $binary_name.exe
