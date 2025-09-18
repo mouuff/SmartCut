d18b51f80eba502a91f83141eefdc1139d64c88b
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-18 16:06:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 
 binary_name=smartcut_windows_amd64.exe
+zip_name=binaries_windows.zip
 go build -ldflags -H=windowsgui cmd/smartcut/main.go
-mv main.exe smartcut_windows_amd64.exe
-rm binaries.zip
-zip -r binaries_windows.zip smartcut_windows_amd64.exe
+mv main.exe $binary_name
+rm $zip_name
+zip -r $zip_name $binary_name
