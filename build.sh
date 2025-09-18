ceb2126a5836bb418a04f8a85ce82621e1337e2c
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-18 16:52:20 +0200

    Update build script and move main.go to root
    Replaced go build command with fyne package in build.sh for Windows packaging, updated executable naming, and moved main.go from cmd/smartcut/ to the project root.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-
-# fyne package -app-id com.mouuff.smartcut -os windows -icon images/icon.png -release
+# go build -ldflags -H=windowsgui cmd/smartcut/main.go
 
 binary_name=smartcut_windows_amd64.exe
 zip_name=binaries_windows.zip
-go build -ldflags -H=windowsgui cmd/smartcut/main.go
-mv main.exe $binary_name
+
+fyne package -app-id com.mouuff.smartcut -os windows -icon images/icon.png -release
+
+mv SmartCuts.exe $binary_name
 rm $zip_name
 zip -r $zip_name $binary_name
