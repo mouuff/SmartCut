4a25e5363b774de25f3e46fb9f7504dc8379d1cd
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-20 14:13:02 +0200

    Secure updates

--- build.sh
+++ build.sh
@@ -1,10 +1,25 @@
 # go build -ldflags -H=windowsgui cmd/smartcut/main.go
 
+private_key="../keys/id_rsa_smartcut"
+public_key="keys/id_rsa.pub"
 binary_name=smartcut_windows_amd64.exe
 zip_name=binaries_windows.zip
+folder_name=binaries_windows
+
+
+rm -f $zip_name
+rm -rf $folder_name
 
 fyne package -app-id com.mouuff.smartcut -os windows -icon images/icon.png -release
 
-mv SmartCut.exe $binary_name
-rm $zip_name
-zip -r $zip_name $binary_name
+mkdir $folder_name
+mv SmartCut.exe $folder_name/$binary_name
+
+
+rocket-update sign -key $private_key -path $folder_name
+rocket-update verify -publicKey $public_key -path $folder_name
+
+cd $folder_name
+zip -r ../$zip_name *
+cd -
+
